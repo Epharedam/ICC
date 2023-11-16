e18e3f1d4b9e064da661635a9b1f28f685fb8f04
Epharedam <hildajoanacg@ciencias.unam.mx>
2023-11-16 15:24:47 -0600

    proyecto 3 update

---
+++ Proyectos/proyecto03/src/Interfaz.java
@@ -0,0 +1,29 @@
+public interface Interfaz {
+
+
+    // Method to add an item to the catalog
+    void agregarPersonal(Item item);
+
+    // Method to remove an item from the catalog
+    void removeItem(Item item);
+
+    // Method to retrieve a list of all items in the catalog
+    List<Item> getAllItems();
+
+    // Method to search for items based on a given keyword
+    List<Item> searchItems(String keyword);
+
+    // Method to get details of a specific item
+    Item getItemDetails(String itemId);
+
+    // Method to update the details of an existing item in the catalog
+    void updateItemDetails(String itemId, Item updatedItem);
+
+    // Method to get the total number of items in the catalog
+    int getTotalItems();
+
+    // Method to clear all items from the catalog
+    void clearCatalog();
+}
+
+}
--- Proyectos/proyecto3/Eventoespectacular/Customer.java
+++
@@ -1,7 +0,0 @@
-public class Customer{
-
-    public void purchaseBook(Book book) {
-        // Logic for purchasing a book
-        System.out.println("Se ha agregado " + book.getTitle());
-    }
-}
