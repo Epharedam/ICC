e3a73b4667f59fdb87fe0411e39409e503757bc3
Epharedam <129038355+Epharedam@users.noreply.github.com>
2023-12-15 11:45:39 -0600

    practica 04 update 1

--- Practicas/Practica04/Main.java
+++ Practicas/Practica04/Main.java
@@ -1,10 +1,21 @@
 public class Main implements AnalizadorCadenas {
     
     public static void main(String[] args) {
-        Main mainInstance = new Main();
+        Main mainPrueba = new Main();
+
+        // Ejemplo de encuentra vocal
         String ejemplo = "ciencias";
-        char primeraVocal = mainInstance.encuentraVocal(ejemplo);
+        char primeraVocal = mainPrueba.encuentraVocal(ejemplo);
         System.out.println("Primera vocal en '" + ejemplo + "' es: " + primeraVocal);
+
+        // Ejemplo de cuadrado
+
+        int nivel = 4; 
+        mainPrueba.cuadrado(nivel);
+
+        String str = "Hilda Carbajal";
+        printInitials(nombre);
+
     }
 
     @Override
@@ -19,11 +30,23 @@ public class Main implements AnalizadorCadenas {
         }
         return '\0';
     }
+
     @Override
     public void cuadrado(int nivel) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cuadrado'");
+        for (int i = 1; i <= nivel; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.print("  ");
+            for (int j = 4; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
+        
+
+        
     @Override
     public void rombosNumericos(int n) {
         // TODO Auto-generated method stub
@@ -31,9 +54,42 @@ public class Main implements AnalizadorCadenas {
     }
     @Override
     public String nombreAbreviado(String nombre) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nombreAbreviado'");
-    }
+        int len = nombre.length();
+ 
+        // to remove any leading or trailing spaces
+        nombre = nombre.trim();
+ 
+        // to store extracted words
+        String t = "";
+        for (int i = 0; i < len; i++) {
+            char ch = nombre.charAt(i);
+ 
+            if (ch != ' ') {
+                // forma la palabra
+                t = t + ch;
+            }
 
-    
+                // primera letra del nombre en mayuscula
+                System.out.print(Character.toUpperCase(t.charAt(0))
+                                 + ". ");
+                t = "";
+            }
+        }
+ 
+        String temp = "";{
+ 
+        for (int j = 0; j < t.length(); j++) {
+ 
+            // primera letrra apellido en mayuscula
+            if (j == 0)
+                temp = temp + Character.toUpperCase(t.charAt(0));
+            // letras en minuscula
+            else
+                temp = temp + Character.toLowerCase(t.charAt(j));
+        }
+
+        System.out.println(temp);
+    }
+   
 }
+
---
+++ Proyectos/proyecto3/proyecto03_Crabajal_Galicia/Trabajador.java
@@ -0,0 +1,17 @@
+class Trabajador {
+    private String nombre;
+    private double salario;
+
+    Trabajador(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
