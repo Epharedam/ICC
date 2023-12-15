d5a939ee45af2fa3404319a532aaaf2e32d9976f
Epharedam <129038355+Epharedam@users.noreply.github.com>
2023-12-15 12:01:36 -0600

    practica 04 update 2

--- Practicas/Practica04/Main.java
+++ Practicas/Practica04/Main.java
@@ -12,9 +12,14 @@ public class Main implements AnalizadorCadenas {
 
         int nivel = 4; 
         mainPrueba.cuadrado(nivel);
+    
+        //Nombre ejemplo
+        String fullName = "Hilda Carbajal";
+        String abbreviatedName = mainPrueba.nombreAbreviado(fullName);
+        System.out.println("" + abbreviatedName);
 
-        String str = "Hilda Carbajal";
-        printInitials(nombre);
+        
+
 
     }
 
@@ -49,47 +54,94 @@ public class Main implements AnalizadorCadenas {
         
     @Override
     public void rombosNumericos(int n) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'rombosNumericos'");
+        int max = 5;
+    int startNumber = 1;
+
+    // First Half of the Pyramid
+    for (int i = 1; i <= max; i++) {
+      // This loop will print the whitespaces in each row
+      // of the first half of the Pyramid
+      for (int j = max; j > i; j--) {
+        System.out.print(" ");
+      }
+      // This loop will print the stars in each row
+      // of the first half of the Pyramid
+      for (int k = 0; k < i * 2 - 1; k++) {
+        System.out.print(startNumber++);
+      }
+      // Re-initializing the start number for next row
+      startNumber = 1;
+      //To move the cursor to new line for each new row
+      System.out.println();
+    }
+
+    // Second Half of the Pyramid
+    for (int i = 1; i <= max - 1; i++) {
+      // Print whitespace in second half of pattern
+      for (int j = 0; j < i; j++) {
+        System.out.print(" ");
+      }
+      // Print stars in second half of pattern
+      for (int k = (max - i) * 2 - 1; k > 0; k--) {
+        System.out.print(startNumber++);
+      }
+      // Re-initializing the start number for next row
+      startNumber = 1;
+      //new line for next row
+      System.out.println();
+    }
+        
     }
     @Override
     public String nombreAbreviado(String nombre) {
         int len = nombre.length();
- 
-        // to remove any leading or trailing spaces
         nombre = nombre.trim();
- 
-        // to store extracted words
+     
         String t = "";
+        String temp = "";
+    
         for (int i = 0; i < len; i++) {
             char ch = nombre.charAt(i);
- 
+    
             if (ch != ' ') {
-                // forma la palabra
                 t = t + ch;
-            }
-
-                // primera letra del nombre en mayuscula
-                System.out.print(Character.toUpperCase(t.charAt(0))
-                                 + ". ");
+            } else {
+    
+                // primera letra del nombre en mayúscula
+                System.out.print(Character.toUpperCase(t.charAt(0)) + ". ");
+                
+                // procesar apellido
+                for (int j = 0; j < t.length(); j++) {
+                    // primera letra del apellido en mayúscula
+                    if (j == 0)
+                        temp = temp + Character.toUpperCase(t.charAt(0));
+                    // letras en minúscula
+                    else
+                        temp = temp + Character.toLowerCase(t.charAt(j));
+                }
+    
+                System.out.println(temp);
                 t = "";
+                temp = "";
             }
         }
- 
-        String temp = "";{
- 
-        for (int j = 0; j < t.length(); j++) {
- 
-            // primera letrra apellido en mayuscula
-            if (j == 0)
-                temp = temp + Character.toUpperCase(t.charAt(0));
-            // letras en minuscula
-            else
-                temp = temp + Character.toLowerCase(t.charAt(j));
-        }
 
-        System.out.println(temp);
+        if (!t.isEmpty()) {
+            System.out.print(Character.toUpperCase(t.charAt(0)) + ". ");
+    
+            for (int j = 0; j < t.length(); j++) {
+                if (j == 0)
+                    temp = temp + Character.toUpperCase(t.charAt(0));
+                else
+                    temp = temp + Character.toLowerCase(t.charAt(j));
+            }
+    
+            System.out.println(temp);
+        }
+    
+        return "";
     }
+    
    
 }
 
